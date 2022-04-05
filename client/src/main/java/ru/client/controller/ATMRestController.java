821ac8609cb7a1516b8d9231795f1b91c9319ea8
VladislavBodikov <krullerr@yandex.ru>
2022-04-05 21:29:14 +0300

    correct client controller response

--- client/src/main/java/ru/client/controller/ATMRestController.java
+++ client/src/main/java/ru/client/controller/ATMRestController.java
@@ -30,51 +30,55 @@ public class ATMRestController {
     private RestTemplate restTemplate;
 
     @PostMapping(path = "/balance", consumes = "application/json")
-    public String balance(@RequestBody AccountDTO accountDTO) {
-        setRestTemplateWithBasicAuth(accountDTO);
+    public String balance(@RequestBody AccountDTO incomeAuthData) {
+        setRestTemplateWithBasicAuth(incomeAuthData);
 
-        ResponseEntity<BalanceDTO> response = requestBalance(accountDTO);
+        ResponseEntity<BalanceDTO> response = requestBalance(incomeAuthData);
 
         return atmService.printBalanceResponse(response);
     }
 
-    private void setRestTemplateWithBasicAuth(AccountDTO accountDTO){
-        String username = accountDTO.getCardNumber();
-        String password = accountDTO.getPinCode();
+    private void setRestTemplateWithBasicAuth(AccountDTO authenticationData){
+        String username = authenticationData.getCardNumber();
+        String password = authenticationData.getPinCode();
         restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(username,password));
     }
 
-    private ResponseEntity<BalanceDTO> requestBalance(AccountDTO accountDTO){
-        HttpEntity<AccountDTO> request = new HttpEntity<>(accountDTO);
-        log.debug("REQUEST : " + request);
+    private ResponseEntity<BalanceDTO> requestBalance(AccountDTO requestAuthData){
+        HttpEntity<AccountDTO> request = new HttpEntity<>(requestAuthData);
+        log.info("REQUEST : " + request);
 
         ResponseEntity<BalanceDTO> response;
         try {
             response = restTemplate.postForEntity(SERVER_URL + BALANCE_URL, request, BalanceDTO.class);
         } catch (RestClientException exception) {
-            log.error(exception.getMessage());
+            log.error("RESPONSE : " + exception.getMessage());
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-        log.debug("RESPONSE : " + response);
+        log.info("RESPONSE : " + response);
         return response;
     }
 
-    @PostMapping(path = "/sendMoney", consumes = "application/json")
+    @PostMapping(path = "/money/transfer", consumes = "application/json")
     public String sendMoney(@RequestBody TransactionDTO transactionDTO){
         setRestTemplateWithBasicAuth(transactionDTO.getAccountFrom());
 
         ResponseEntity<BalanceDTO> balanceBeforeTransfer = requestBalance(transactionDTO.getAccountFrom());
-        ResponseEntity<BalanceDTO> balanceAfterTransfer;
+        boolean isRequestBalanceFailure = balanceBeforeTransfer.getStatusCode() == HttpStatus.CONFLICT
+                                || balanceBeforeTransfer.getBody() == null;
+        if (isRequestBalanceFailure){
+            log.error("\nFailed request balance from server with data: " + transactionDTO + "\n");
+            return "\nWRONG PIN-CODE\n";
+        }
 
         boolean isEnoughBalanceForTransfer = balanceBeforeTransfer.getBody().getAmount()
-                .subtract(transactionDTO.getAmountToTransfer())
-                .compareTo(new BigDecimal(0)) >= 0;
-
+                                                .subtract(transactionDTO.getAmountToTransfer())
+                                                .compareTo(new BigDecimal(0)) >= 0;
         if (isEnoughBalanceForTransfer){
-            balanceAfterTransfer = transfer(transactionDTO);
+            ResponseEntity<BalanceDTO> balanceAfterTransfer = transfer(transactionDTO);
             return atmService.printResultOfTransaction(balanceBeforeTransfer,balanceAfterTransfer,transactionDTO.getAmountToTransfer());
         }
-        return "\nDon`t have enough money to transfer!\n";
+        return "\nTransaction denied! \nDon`t have enough money to transfer!\n";
     }
 
     private ResponseEntity<BalanceDTO> transfer(TransactionDTO transactionDTO){
